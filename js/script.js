892da45d37ba36689424f18be9f9c314eaad93d3
mohamadMehdiDabestani <mohamad1382mhd@gmail.com>
2022-07-04 08:05:17 -0700

    responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const pBackToTop = document.querySelector(".back-top p");
 const btnBackToTop = document.querySelector(".back-top button");
 const contactUsForm = document.querySelector(".contactus-form");
+const responsiveMenuIcon = document.querySelector(".responsive-menu-icon");
+const responsiveMenu = document.querySelector(".responsive-menu");
+
 const backToTop = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
@@ -15,6 +18,10 @@ requiredFields.forEach((el) =>
     let invalidations = Array.from(requiredFields).filter(
       (el) => el.value.length === 0
     );
+    if (e.target.value.length > 0) {
+      console.log("if");
+      e.target.classList.remove("error-validation");
+    }
     let submitBtn = document.querySelector(".contactus-form button");
     if (invalidations.length === 0) {
       submitBtn.classList.remove("disable");
@@ -30,9 +37,19 @@ document.querySelector(".contactus-form").addEventListener("submit", (e) => {
     (el) => el.value.length === 0
   );
   if (invalidations.length > 0) {
-    invalidations.forEach((el) =>{
-        el.classList.add('error-validation')
-    })
+    invalidations.forEach((el) => {
+      el.classList.add("error-validation");
+    });
     e.preventDefault();
   }
+});
+responsiveMenuIcon.addEventListener("click", (e) => {
+  document.querySelector(".responsive-menu").classList.add("active");
+  document.querySelector("body").style.overflowY = "hidden";
+  
+});
+responsiveMenu.addEventListener("click", (e) => {
+  document.querySelector(".responsive-menu").classList.remove("active");
+  document.querySelector("body").style.overflowY = "scroll";
+  
 });
